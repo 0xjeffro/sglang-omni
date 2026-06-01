36ed04881527948c9705ba4b88071a349baeee82
zhaochenyang20 <zhaochen20@outlook.com>
2026-06-01 17:08:08 +0000

    fix lijnt

--- sglang_omni/models/qwen3_asr/audio_lengths.py
+++ sglang_omni/models/qwen3_asr/audio_lengths.py
@@ -18,11 +18,7 @@ def qwen3_asr_audio_token_lengths(input_lengths: Any) -> torch.Tensor:
         input_lengths = torch.tensor(input_lengths)
     input_lengths_leave = input_lengths % 100
     feat_lengths = (input_lengths_leave - 1) // 2 + 1
-    return (
-        ((feat_lengths - 1) // 2 + 1 - 1) // 2
-        + 1
-        + (input_lengths // 100) * 13
-    )
+    return ((feat_lengths - 1) // 2 + 1 - 1) // 2 + 1 + (input_lengths // 100) * 13
 
 
 def qwen3_asr_num_audio_tokens(num_mel_frames: int) -> int:
